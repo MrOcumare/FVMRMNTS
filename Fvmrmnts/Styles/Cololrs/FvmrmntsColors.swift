dde55503ce464be59d271757a050799e92dad804
Mr Ocumare <ilyaocumare@gmail.com>
2019-07-22 17:52:37 +0300

    old version

---
+++ Fvmrmnts/Styles/Cololrs/FvmrmntsColors.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  Fvmrmnts
+//
+//  Created by Mr.Ocumare on 18/07/2019.
+//  Copyright © 2019 Ilya Ocumare. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    struct Fvmrmnts {
+        struct Color {
+            static let Black = UIColor(netHex: 0x000000)
+            static let White = UIColor(netHex: 0xFFFFFF)
+            static let Violet = UIColor(netHex: 0x61389F)
+            static let Red = UIColor(netHex: 0xD83F3F)
+            static let Gray = UIColor(netHex: 0xA0A0A0)
+        }
+    }
+}
+
