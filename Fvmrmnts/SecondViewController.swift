e128a17a1f0413ae17b7f8964e1b4a0371cccb9b
Mr Ocumare <ilyaocumare@gmail.com>
2019-08-09 15:46:47 +0300

    almost done animation

--- Fvmrmnts/SecondViewController.swift
+++ Fvmrmnts/SecondViewController.swift
@@ -27,7 +27,7 @@ class SecondViewController: UIViewController {
     lazy var isExit = false
     
     
-    let showLabel : UILabel = {
+    lazy var showLabel : UILabel = {
         let label = UILabel()
         label.font = UIFont(name: "GTWalsheimProBold", size: 64)
         label.numberOfLines = 0
@@ -37,7 +37,7 @@ class SecondViewController: UIViewController {
         return label
     }()
     
-    let collectonView : UICollectionView = {
+    lazy var collectonView : UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.minimumLineSpacing = 40
         let collectonView = UICollectionView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), collectionViewLayout: layout)
@@ -61,25 +61,31 @@ class SecondViewController: UIViewController {
        
         
         view.addSubview(collectonView)
-        collectonView.addSubview(showLabel)
+        view.addSubview(showLabel)
         setUPshowLabel()
         
         setupCollectionView()
         
     }
+    var collectionViewTopAnchor : NSLayoutConstraint?
     
     func setupCollectionView() {
         collectonView.translatesAutoresizingMaskIntoConstraints = false
         collectonView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 92).isActive = true
         collectonView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -92).isActive = true
-        collectonView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        collectionViewTopAnchor = collectonView.topAnchor.constraint(equalTo: view.topAnchor, constant: 237)
+        collectionViewTopAnchor?.isActive = true
         collectonView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
     }
+    
+    
+    var showLabelTopAnchor : NSLayoutConstraint?
     
     func setUPshowLabel() {
         showLabel.translatesAutoresizingMaskIntoConstraints = false
         showLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 140).isActive = true
-        showLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 96).isActive = true
+        showLabelTopAnchor = showLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 96)
+        showLabelTopAnchor?.isActive = true
     }
     
 }
@@ -119,33 +125,33 @@ extension SecondViewController: UICollectionViewDelegate, UICollectionViewDataSo
     }
 
     @objc func menuButtonAction(recognizer:UITapGestureRecognizer) {
-       
         if isExit {
            self.delegate?.navigateToFirstPage()
+        } else {
+            isExit = true
+            self.handleAnimate(collectionViewAnimation: 0, showLabelAnimation: -96)
             
         }
-        collectonView.contentInset = UIEdgeInsets(top: 97, left: 0, bottom: 0, right: 0)
-        showLabel.isHidden = false
-        isExit = true
+       
         
-//        self.dismiss(animated: true, completion: nil)
     }
-    
     
     func collectionView(_ collectionView: UICollectionView, didUpdateFocusIn context: UICollectionViewFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
         if context.nextFocusedIndexPath!.item == 0 {
-            collectonView.contentInset = UIEdgeInsets(top: 237, left: 0, bottom: 0, right: 0)
+            handleAnimate(collectionViewAnimation: 237, showLabelAnimation: 96)
             isExit = true
-            showLabel.isHidden = false
-            
         } else {
-            
-            showLabel.isHidden = true
-            collectonView.contentInset = UIEdgeInsets(top: 97, left: 0, bottom: 0, right: 0)
+            handleAnimate(collectionViewAnimation: 0, showLabelAnimation: -96)
             isExit = false
-            
         }
-        
+    }
+    
+    func handleAnimate(collectionViewAnimation: CGFloat, showLabelAnimation: CGFloat) {
+        showLabelTopAnchor?.constant = showLabelAnimation
+        collectionViewTopAnchor?.constant = collectionViewAnimation
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
     }
     
     func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
@@ -159,12 +165,6 @@ extension SecondViewController: UICollectionViewDelegate, UICollectionViewDataSo
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: self.collectonView.frame.width, height: 240)
     }
-    
-    
-    
-    
-
-    
 }
 
 
