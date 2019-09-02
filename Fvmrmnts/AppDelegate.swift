b0959f78b0b118752f2491e1b6c7e9b9c78d9cc0
Mr Ocumare <ilyaocumare@gmail.com>
2019-09-02 11:37:57 +0300

    DONE

--- Fvmrmnts/AppDelegate.swift
+++ Fvmrmnts/AppDelegate.swift
@@ -35,23 +35,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController()
-        
+      
         // Initialise the first coordinator with the main navigation controller
-        firstcoordinator = FirstCoordinator(navigationController: window?.rootViewController as! UINavigationController)
+       
         
         // The start method will actually display the main view
-        firstcoordinator?.start()
+      
          print("----exi767t")
         
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
+//    var bgTask : UIBackgroundTaskIdentifier!
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
-         print("----exi767dsdfsfsdt")
-        
+//        bgTask = application.beginBackgroundTask(expirationHandler: { () -> Void in
+//            application.endBackgroundTask(self.bgTask)
+//            self.bgTask = UIBackgroundTaskIdentifier.invalid
+//        })
+//
+//        DispatchQueue.global(qos : .background).async() { () -> Void in
+//            self.bgTask = UIBackgroundTaskIdentifier.invalid
+//            application.endBackgroundTask(self.bgTask)
+//        }
+//         print("----exi767dsdfsfsdt")
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
